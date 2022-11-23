779f56ae939656f321d7f2c74a46bf5c70ee6799
Edison <princehan920453024@gmail.com>
2022-11-23 15:26:45 -0500

    mount both code and data to docker image.

--- image/docker_run.sh
+++ image/docker_run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 echo "Loading docker image ..."
-docker load -i itac_davis-purdue.tar
+docker load -i image/itac_davis-purdue.tar
 
 echo "Current available docker images: "
 docker images
 
 echo "Run the loaded image ..."
-cd ..
 PWD=$(pwd)
 echo "Any changes made in run.sh should be in image/run.sh, instead of code/docker/run.sh"
 cp ${PWD}/image/run.sh ${PWD}/code/docker/run.sh
 chmod +x ${PWD}/code/docker/run.sh
-docker run -v ${PWD}/code:/code --gpus all itac_davis-purdue:v1 docker/run.sh
+docker run -v ${PWD}/code:/code -v ${PWD}/data:/data --gpus all itac_davis-purdue:v1 docker/run.sh
 
 echo "Image exited!"
